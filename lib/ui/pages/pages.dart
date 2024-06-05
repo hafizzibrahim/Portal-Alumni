37c010af9a23122da249257138615b911844f2a7
Ahmad Nurdin <nurnurdinnur11@gmail.com>
2024-06-05 16:36:33 +0700

    Update pages.dart

--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -1,7 +1,12 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:portal_alumni_v1/shared/shared.dart';
 import 'package:portal_alumni_v1/ui/widgets/widgets.dart';
 
+part 'home_page.dart';
 part 'login_page.dart';
+part 'profile_page.dart';
 part 'main_page.dart';
+part 'career_page.dart';
