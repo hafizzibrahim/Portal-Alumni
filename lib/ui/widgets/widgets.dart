986ba57fe11c5be222bc3d55ecbc0b80bb60bc78
Hafiz <hafizzibrahimm0@gmail.com>
2024-06-05 11:03:36 +0700

    -add part widget button and text field

---
+++ lib/ui/widgets/widgets.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:portal_alumni_v1/shared/shared.dart';
+
+part 'custom_text_field_widget.dart';
+part 'custom_button_widget.dart';
