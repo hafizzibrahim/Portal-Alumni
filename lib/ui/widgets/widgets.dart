95f8d3bce8c3046cdc4443e63d235f5ddfa295d0
Ahmad Nurdin <nurnurdinnur11@gmail.com>
2024-06-05 16:36:42 +0700

    Update widgets.dart

--- lib/ui/widgets/widgets.dart
+++ lib/ui/widgets/widgets.dart
@@ -1,6 +1,8 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:portal_alumni_v1/shared/shared.dart';
 
+part 'article_wigets.dart';
 part 'custom_text_field_widget.dart';
 part 'custom_button_widget.dart';
+part 'search_textfield_widget.dart';
