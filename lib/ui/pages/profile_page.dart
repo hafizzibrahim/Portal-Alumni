9bd4edb36dd0068f3bcee866c331cfc8701df3df
Ahmad Nurdin <nurnurdinnur11@gmail.com>
2024-06-06 10:27:20 +0700

    Fix profile_page.dart

--- lib/ui/pages/profile_page.dart
+++ lib/ui/pages/profile_page.dart
@@ -7,73 +7,66 @@ class ProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: whiteblueColor,
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(230),
-        child: Container(
-          decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(50),
-                  bottomRight: Radius.circular(50)),
-              gradient: LinearGradient(
-                  colors: [Color(0xFF5356FF), Color(0xFF221FBB)],
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter)),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-            child: Column(
-              children: <Widget>[
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.arrow_back_ios_rounded,
-                      size: 35,
-                      color: whiteColor,
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Image.asset(
-                  'assets/images/profile_photo.png',
-                  scale: 1.5,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  'Mulyadi',
-                  style:
-                      whiteTextStyle.copyWith(fontSize: 16, fontWeight: bold),
-                ),
-                Text(
-                  '33372xxxxx@untirta.ac.id',
-                  style: whiteTextStyle.copyWith(
-                      fontSize: 13, fontWeight: reguler),
-                )
-              ],
-            ),
-          ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            profileHeader(context),
+            const SizedBox(height: 40,),
+            textField('Nama Lengkap', 'Mulyadi'),
+            textField('Nim', '33372xxxxx'),
+            textField('Fakultas', 'Teknik'),
+            textField('Jurusan', 'Informatika'),
+            textField('Tahun Lulus', '2025'),
+          ],
         ),
       ),
-      body: ListView(
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-        children: [
-          textField('Nama Lengkap', 'Mulyadi'),
-          textField('Nim', '33372xxxxx'),
-          textField('Fakultas', 'Teknik'),
-          textField('Jurusan', 'Informatika'),
-          textField('Tahun Lulus', '2025'),
-        ],
+    );
+  }
+
+  Widget profileHeader(BuildContext context){
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      decoration: const BoxDecoration(
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(50),
+              bottomRight: Radius.circular(50)),
+          gradient: LinearGradient(
+              colors: [Color(0xFF5356FF), Color(0xFF221FBB)],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter)),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 50, bottom: 20),
+        child: Column(
+          children: <Widget>[
+            const SizedBox(
+              height: 10,
+            ),
+            Image.asset(
+              'assets/images/profile_photo.png',
+              scale: 1.5,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Mulyadi',
+              style:
+              whiteTextStyle.copyWith(fontSize: 16, fontWeight: bold),
+            ),
+            Text(
+              '33372xxxxx@untirta.ac.id',
+              style: whiteTextStyle.copyWith(
+                  fontSize: 13, fontWeight: reguler),
+            )
+          ],
+        ),
       ),
     );
   }
 
   Widget textField(String title, String name) {
     return Padding(
-      padding: const EdgeInsets.all(10),
+      padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 5),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
