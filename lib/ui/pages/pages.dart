44499531c4fe695a1acddd47a5d8c364a869a572
Ahmad Nurdin <nurnurdinnur11@gmail.com>
2024-06-07 15:29:42 +0700

    Update pages.dart

--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:portal_alumni_v1/shared/shared.dart';
 import 'package:portal_alumni_v1/ui/widgets/widgets.dart';
+import 'package:readmore/readmore.dart';
 
 part 'home_page.dart';
 part 'login_page.dart';
@@ -11,3 +12,7 @@ part 'profile_page.dart';
 part 'main_page.dart';
 part 'career_page.dart';
 part 'article_page.dart';
+part 'tracer_pekerjaan_page.dart';
+part 'tracer_biodata_page.dart';
+part 'tracer_wisuda_page.dart';
+part 'career_detail_page.dart';
