e91d1886d43aff57f7c17222a6e40a0d7b46aebb
Hafiz <hafizzibrahimm0@gmail.com>
2024-06-14 09:42:01 +0700

    -Update add login logic

--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -1,7 +1,39 @@
 part of 'pages.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  Future<void> _login() async {
+    final email = _emailController.text;
+    final password = _passwordController.text;
+    try {
+      await login(email, password);
+      Navigator.pushNamed(context, '/main-page');
+    } catch (e) {
+      print('Login Error: $e');
+      showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+                title: Text('Login Failed'),
+                content: Text(e.toString()),
+                actions: <Widget>[
+                  TextButton(
+                      onPressed: () {
+                        Navigator.of(ctx).pop();
+                      },
+                      child: Text('Okay'))
+                ],
+              ));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +43,7 @@ class LoginPage extends StatelessWidget {
         child: Container(
           decoration: BoxDecoration(
             borderRadius:
-            const BorderRadius.vertical(bottom: Radius.circular(30)),
+                const BorderRadius.vertical(bottom: Radius.circular(30)),
             color: blueColor,
           ),
           child: Stack(
@@ -24,19 +56,24 @@ class LoginPage extends StatelessWidget {
               Positioned(
                   top: 30,
                   right: 0,
-                  child: Image.asset('assets/images/img_untirta_outlined.png', scale: 2,))
+                  child: Image.asset(
+                    'assets/images/img_untirta_outlined.png',
+                    scale: 2,
+                  ))
             ],
           ),
         ),
       ),
       body: ListView(
         children: [
-
           Padding(
             padding: const EdgeInsets.only(left: 25, right: 25, top: 25),
             child: Column(
               children: [
-                Image.asset('assets/icons/ic_logo_untirta.png', scale: 1.5,),
+                Image.asset(
+                  'assets/icons/ic_logo_untirta.png',
+                  scale: 1.5,
+                ),
                 const SizedBox(
                   height: 24,
                 ),
@@ -53,18 +90,30 @@ class LoginPage extends StatelessWidget {
                 const SizedBox(
                   height: 24,
                 ),
-                TextfieldWidget(label: 'Email', obscureText: false, icon: const Icon(Icons.email_rounded),),
+                TextfieldWidget(
+                  controller: _emailController,
+                  label: 'Email',
+                  obscureText: false,
+                  icon: const Icon(Icons.email_rounded),
+                ),
                 const SizedBox(
                   height: 16,
                 ),
-                TextfieldWidget(label: 'Password', obscureText: true, icon: const Icon(Icons.lock),),
+                TextfieldWidget(
+                  controller: _passwordController,
+                  label: 'Password',
+                  obscureText: true,
+                  icon: const Icon(Icons.lock),
+                ),
                 const SizedBox(
                   height: 32,
                 ),
                 CustomButtonWidget(
                   title: 'Masuk',
                   width: 250,
-                  heigth: 55, isEnable: true, onTap: () { Navigator.pushNamed(context, '/main-page'); },
+                  heigth: 55,
+                  isEnable: true,
+                  onTap: _login,
                 )
               ],
             ),
@@ -75,19 +124,47 @@ class LoginPage extends StatelessWidget {
   }
 }
 
+Future<void> login(String email, String password) async {
+  final url = '#########################';
+  final response = await http.post(
+    Uri.parse(url),
+    headers: {'Content-Type': 'aplication/json'},
+    body: jsonEncode({'email': email, 'password': password}),
+  );
+
+  if (response.statusCode == 200) {
+    final responseData = jsonDecode(response.body);
+    if (responseData['data'] != null) {
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setString('token', responseData['data']['token']);
+      await prefs.setString('userId', responseData['data']['id']);
+      await prefs.setString('email', responseData['data']['email']);
+      print('Login successful');
+    } else {
+      throw Exception('Failed to parse response');
+    }
+  } else {
+    print('Response Status Code: ${response.statusCode}');
+    print('Response Body: ${response.body}');
+    throw Exception('Failed to login');
+  }
+}
+
 class TextfieldWidget extends StatefulWidget {
   final String label;
   final bool obscureText;
   late bool isVisible;
   final Icon icon;
   final VoidCallback? onTap;
+  final TextEditingController? controller;
+
   TextfieldWidget(
       {super.key,
-        required this.label,
-        this.obscureText = true,
-        this.isVisible = true,
-        this.onTap,
-        required this.icon});
+      required this.label,
+      this.obscureText = true,
+      this.isVisible = true,
+      this.onTap,
+      required this.icon, this.controller});
 
   @override
   State<TextfieldWidget> createState() => _TextfieldWidgetState();
@@ -101,8 +178,9 @@ class _TextfieldWidgetState extends State<TextfieldWidget> {
         SizedBox(
           //height: 45,
           child: TextFormField(
+            controller: widget.controller,
             obscureText:
-            widget.isVisible ? widget.obscureText : widget.isVisible,
+                widget.isVisible ? widget.obscureText : widget.isVisible,
             decoration: InputDecoration(
                 focusedBorder: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(14),
@@ -116,20 +194,20 @@ class _TextfieldWidgetState extends State<TextfieldWidget> {
                 prefixIcon: widget.icon,
                 suffixIcon: widget.obscureText
                     ? GestureDetector(
-                    onTap: () {
-                      if (widget.isVisible == false) {
-                        setState(() {
-                          widget.isVisible = true;
-                        });
-                      } else {
-                        setState(() {
-                          widget.isVisible = false;
-                        });
-                      }
-                    },
-                    child: Icon(widget.isVisible
-                        ? Icons.visibility_off
-                        : Icons.visibility))
+                        onTap: () {
+                          if (widget.isVisible == false) {
+                            setState(() {
+                              widget.isVisible = true;
+                            });
+                          } else {
+                            setState(() {
+                              widget.isVisible = false;
+                            });
+                          }
+                        },
+                        child: Icon(widget.isVisible
+                            ? Icons.visibility_off
+                            : Icons.visibility))
                     : null),
           ),
         ),
