00d1aea61d18a94f9fc923c33a4068f84c7731a4
Hafiz Ibrahim <hafizzibrahimm0@gmail.com>
2024-06-19 14:54:52 +0700

    -update main.dart (add bypass SSL)

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,62 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:portal_alumni_v1/shared/shared.dart';
 import 'package:portal_alumni_v1/ui/pages/pages.dart';
+import 'package:portal_alumni_v1/viewmodels/login_viewmodel.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  // Bypass SSL verification (for development purposes only)
+  HttpOverrides.global = MyHttpOverrides();
+  runApp(MyApp());
+}
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
+    return ChangeNotifierProvider(
+      create: (context) => LoginViewModel(),
+      child: MaterialApp(
+        title: 'Portal Alumni',
+        theme: ThemeData(
           scaffoldBackgroundColor: lightBackgroundColor,
           appBarTheme: AppBarTheme(
-              backgroundColor: whiteColor,
-              elevation: 0,
-              iconTheme: IconThemeData(color: blackColor),
-              centerTitle: true,
-              titleTextStyle:
-              blackTextStyle.copyWith(fontSize: 20, fontWeight: semiBold)
-          )
+            backgroundColor: whiteColor,
+            elevation: 0,
+            iconTheme: IconThemeData(color: blackColor),
+            centerTitle: true,
+            titleTextStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.w600, color: Colors.black),
+          ),
+        ),
+        initialRoute: '/login-page', // Set initialRoute to your login page
+        routes: {
+          '/login-page': (context) => LoginPage(),
+          '/main-page': (context) => MainPage(),
+          '/home-page': (context) => const HomePage(),
+          '/profile-page': (context) => const ProfilePage(),
+          '/article-page': (context) => const ArticlePage(),
+          '/article-detail-page': (context) => const ArticleDetailPage(),
+          '/dashboard-page': (context) => const DashboardPage(),
+          '/career-page': (context) => const CareerPage(),
+          '/career-detail-page': (context) => const CareerDetailPage(),
+          '/tracer-study-page': (context) => const TracerStudyPage(),
+          '/tracer-biodata-page': (context) => const TracerBiodataPage(),
+          '/tracer-wisuda-page': (context) => const TracerWisudaPage(),
+          '/tracer-pekerjaan-page': (context) => const TracerPekerjaanPage(),
+          '/tracer-studi-lanjut-page': (context) => const TracerStudiLanjutPage(),
+          '/tracer-study-selesai-page': (context) => const TracerStudySelesaiPage(),
+        },
       ),
-      routes: {
-        '/': (context) =>  LoginPage(),
-
-        '/login-page': (context) => const LoginPage(),
-        '/main-page': (context) => MainPage(),
-        '/home-page' : (context) => const HomePage(),
-        '/profile-page' : (context) => const ProfilePage(),
-        '/article-page' : (context) => const ArticlePage(),
-        '/article-detail-page' : (context) => const ArticleDetailPage(),
-        '/dashboard-page' : (context) => const DashboardPage(),
-        '/career-page' : (context) => const CareerPage(),
-        '/career-detail-page' : (context) => const CareerDetailPage(),
-
-        '/tracer-study-page' : (context) => const TracerStudyPage(),
-        '/tracer-biodata-page' : (context) => const TracerBiodataPage(),
-        '/tracer-wisuda-page' : (context) => const TracerWisudaPage(),
-        '/tracer-pekerjaan-page' : (context) => const TracerPekerjaanPage(),
-        '/tracer-studi-lanjut-page' : (context) => const TracerStudiLanjutPage(),
-        '/tracer-study-selesai-page' : (context) => const TracerStudySelesaiPage(),
-
-      },
     );
   }
 }
