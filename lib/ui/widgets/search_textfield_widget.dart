5413edb01652f9434aa17074f775344bff9ee08b
Hafiz Ibrahim <hafizzibrahimm0@gmail.com>
2024-06-20 14:50:56 +0700

    -update search bar

--- lib/ui/widgets/search_textfield_widget.dart
+++ lib/ui/widgets/search_textfield_widget.dart
@@ -5,7 +5,16 @@ class SearchTextFieldWidget extends StatelessWidget {
   final Icon? prefixIcon;
   final Icon? suffixIcon;
   final String labelText;
-  const SearchTextFieldWidget({super.key, this.fillColor = const Color(0xffffffff), this.suffixIcon, required this.labelText, this.prefixIcon});
+  final ValueChanged<String>? onChanged; // Tambahkan parameter onChanged
+
+  const SearchTextFieldWidget({
+    super.key,
+    this.fillColor = const Color(0xffffffff),
+    this.suffixIcon,
+    required this.labelText,
+    this.prefixIcon,
+    this.onChanged, // Tambahkan onChanged ke konstruktor
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -15,11 +24,12 @@ class SearchTextFieldWidget extends StatelessWidget {
         fillColor: fillColor,
         prefixIcon: prefixIcon,
         suffixIcon: suffixIcon,
-        labelText: labelText,
+        hintText: labelText,
         border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(14),
             borderSide: BorderSide.none),
       ),
+      onChanged: onChanged, // Gunakan onChanged di sini
     );
   }
 }
