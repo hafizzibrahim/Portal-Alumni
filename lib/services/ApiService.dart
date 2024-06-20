91c515d77c1025a70e58153a15c90757e2f4b9e0
Davidwae <davidspt.baru@gmail.com>
2024-06-20 15:50:50 +0700

    - adding function getArticle

--- lib/services/ApiService.dart
+++ lib/services/ApiService.dart
@@ -5,6 +5,8 @@ import '../models/SurveyModel.dart';
 import '../models/UserModel.dart';
 import 'package:http/http.dart' as http;
 
+import '../models/article_model.dart';
+
 class ApiService {
   static const String baseUrl = "https://helloworld1978.my.id/api";
 
@@ -42,6 +44,18 @@ class ApiService {
       return jsonResponse.map((job) => Job.fromJson(job)).toList();
     } else {
       throw Exception('Failed to load jobs');
+    }
+  }
+
+  Future<List<ArticleModel>> getArticle() async {
+    final String url = '$baseUrl/articles';
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      List jsonResponse = json.decode(response.body)['data'];
+      return jsonResponse.map((articles) => ArticleModel.fromJson(articles)).toList();
+    } else {
+      throw Exception('Failed to load articles');
     }
   }
 }
