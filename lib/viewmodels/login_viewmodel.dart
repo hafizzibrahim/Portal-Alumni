6596606a31ac805bf092695ae375968ea8b4bd8d
rifqifhrz <rifqifahrezi310@gmail.com>
2024-06-24 11:25:52 +0700

    - Update login_viewmodel.dart to get current user

--- lib/viewmodels/login_viewmodel.dart
+++ lib/viewmodels/login_viewmodel.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import '../models/UserModel.dart';
 import '../services/ApiService.dart';
+
+import 'package:flutter/material.dart';
+// Ganti dengan path yang sesuai
 
 class LoginViewModel extends ChangeNotifier {
   final ApiService apiService = ApiService();
@@ -10,7 +14,7 @@ class LoginViewModel extends ChangeNotifier {
     try {
       final response = await apiService.loginUser(email, password);
 
-      // Save user data to SharedPreferences if login successful
+      // Simpan data pengguna ke SharedPreferences jika login berhasil
       final prefs = await SharedPreferences.getInstance();
       await prefs.setString('token', response.token);
       await prefs.setString('userId', response.id);
@@ -31,5 +35,18 @@ class LoginViewModel extends ChangeNotifier {
 
     print('Logout successful');
     notifyListeners();
+  }
+
+  Future<UserModel> getCurrentUser() async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('token');
+    final userId = prefs.getString('userId');
+    final email = prefs.getString('email');
+
+    if (token != null && userId != null && email != null) {
+      return UserModel(id: userId, email: email, token: token);
+    } else {
+      throw Exception('User not logged in');
+    }
   }
 }
