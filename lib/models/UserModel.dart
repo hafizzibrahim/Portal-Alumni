3d3bf3d103e0e51b0cc50dd4c8033428b8799ed5
Hafiz Ibrahim <hafizzibrahimm0@gmail.com>
2024-06-25 11:50:18 +0700

    -update

--- lib/models/UserModel.dart
+++ lib/models/UserModel.dart
@@ -1,11 +1,21 @@
 class UserModel {
   final String id;
   final String email;
+  final int nim;
+  final String fakultas;
+  final String jurusan;
+  final int tahunLulus;
+  final String imagePath;
   final String token;
 
   UserModel({
     required this.id,
     required this.email,
+    required this.nim,
+    required this.fakultas,
+    required this.jurusan,
+    required this.tahunLulus,
+    required this.imagePath,
     required this.token,
   });
 
@@ -13,6 +23,11 @@ class UserModel {
     return UserModel(
       id: json['data']['id'],
       email: json['data']['email'],
+      nim: json['data']['nim'],
+      fakultas: json['data']['fakultas'],
+      jurusan: json['data']['jurusan'],
+      tahunLulus: json['data']['tahun_lulus'],
+      imagePath: json['data']['image_path'],
       token: json['data']['token'],
     );
   }
