849aafe5ca155011aa4c9afcfd608273741e33ec
Hafiz Ibrahim <hafizzibrahimm0@gmail.com>
2024-06-25 11:48:53 +0700

    -update login view model

--- lib/viewmodels/login_viewmodel.dart
+++ lib/viewmodels/login_viewmodel.dart
@@ -4,9 +4,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 import '../models/UserModel.dart';
 import '../services/ApiService.dart';
 
-import 'package:flutter/material.dart';
-// Ganti dengan path yang sesuai
-
 class LoginViewModel extends ChangeNotifier {
   final ApiService apiService = ApiService();
 
@@ -19,6 +16,11 @@ class LoginViewModel extends ChangeNotifier {
       await prefs.setString('token', response.token);
       await prefs.setString('userId', response.id);
       await prefs.setString('email', response.email);
+      await prefs.setInt('nim', response.nim);
+      await prefs.setString('fakultas', response.fakultas);
+      await prefs.setString('jurusan', response.jurusan);
+      await prefs.setInt('tahun_lulus', response.tahunLulus);
+      await prefs.setString('image_path', response.imagePath);
 
       print('Login successful');
       notifyListeners();
@@ -42,9 +44,23 @@ class LoginViewModel extends ChangeNotifier {
     final token = prefs.getString('token');
     final userId = prefs.getString('userId');
     final email = prefs.getString('email');
+    final nim = prefs.getInt('nim');
+    final fakultas = prefs.getString('fakultas');
+    final jurusan = prefs.getString('jurusan');
+    final tahunLulus = prefs.getInt('tahun_lulus');
+    final imagePath = prefs.getString('image_path');
 
-    if (token != null && userId != null && email != null) {
-      return UserModel(id: userId, email: email, token: token);
+    if (token != null && userId != null && email != null && nim != null && fakultas != null && jurusan != null && tahunLulus != null && imagePath != null) {
+      return UserModel(
+        id: userId,
+        email: email,
+        nim: nim,// Sesuaikan dengan atribut yang sesuai
+        fakultas: fakultas, // Sesuaikan dengan atribut yang sesuai
+        jurusan: jurusan, // Sesuaikan dengan atribut yang sesuai
+        tahunLulus: tahunLulus, // Sesuaikan dengan atribut yang sesuai
+        imagePath: imagePath, // Sesuaikan dengan atribut yang sesuai
+        token: token,
+      );
     } else {
       throw Exception('User not logged in');
     }
