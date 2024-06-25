0e5b490e766063e9bbf99c1b14416fa2e9aa2e12
Hafiz Ibrahim <hafizzibrahimm0@gmail.com>
2024-06-25 11:49:15 +0700

    -update profile

--- lib/ui/pages/profile_page.dart
+++ lib/ui/pages/profile_page.dart
@@ -1,5 +1,4 @@
 part of 'pages.dart';
-// Ganti dengan path yang sesuai
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
@@ -15,7 +14,7 @@ class ProfilePage extends StatelessWidget {
               future: loginViewModel.getCurrentUser(),
               builder: (context, snapshot) {
                 if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Center(child: CircularProgressIndicator());
+                  return const Center(child: CircularProgressIndicator());
                 } else if (snapshot.hasError) {
                   return Center(child: Text('Error: ${snapshot.error}'));
                 } else {
@@ -24,11 +23,11 @@ class ProfilePage extends StatelessWidget {
                     children: [
                       profileHeader(currentUser),
                       SizedBox(height: 40),
-                      textField('Nama Lengkap', 'Mulyadi'), // Ganti dengan data dari API jika tersedia
-                      textField('Nim', '33372xxxxx'), // Ganti dengan data dari API jika tersedia
-                      textField('Fakultas', 'Teknik'), // Ganti dengan data dari API jika tersedia
-                      textField('Jurusan', 'Informatika'), // Ganti dengan data dari API jika tersedia
-                      textField('Tahun Lulus', '2025'), // Ganti dengan data dari API jika tersedia
+                      textField('Nama Lengkap', 'Kosyong'),
+                      textField('Nim', currentUser.nim.toString()),
+                      textField('Fakultas', currentUser.fakultas),
+                      textField('Jurusan', currentUser.jurusan),
+                      textField('Tahun Lulus', currentUser.tahunLulus.toString()),
                       SizedBox(height: 16),
                       CustomButtonWidget(
                         title: 'Keluar',
@@ -47,6 +46,7 @@ class ProfilePage extends StatelessWidget {
       ),
     );
   }
+
 
   void _logout(BuildContext context) async {
     await Provider.of<LoginViewModel>(context, listen: false).logout();
@@ -72,18 +72,19 @@ class ProfilePage extends StatelessWidget {
         child: Column(
           children: <Widget>[
             SizedBox(height: 10),
-            Image.asset(
-              'assets/images/profile_photo.png',
+            // Menampilkan gambar dari URL
+            Image.network(
+              user.imagePath,
               scale: 1.5,
             ),
             SizedBox(height: 20),
             Text(
-              'Mulyadi', // Ganti dengan nama dari data pengguna
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white), // Sesuaikan dengan gaya teks yang Anda inginkan
+              'user.nama', // Menampilkan nama dari data pengguna
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white),
             ),
             Text(
               user.email, // Menampilkan email dari data pengguna
-              style: TextStyle(fontSize: 13, fontWeight: FontWeight.normal, color: Colors.white), // Sesuaikan dengan gaya teks yang Anda inginkan
+              style: TextStyle(fontSize: 13, fontWeight: FontWeight.normal, color: Colors.white),
             )
           ],
         ),
@@ -91,7 +92,8 @@ class ProfilePage extends StatelessWidget {
     );
   }
 
-  Widget textField(String title, String value) {
+
+  Widget textField(String title, String value, {String? initialValue}) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 5),
       child: Column(
@@ -99,19 +101,19 @@ class ProfilePage extends StatelessWidget {
         children: [
           Text(
             title,
-            style: TextStyle(fontSize: 13, fontWeight: FontWeight.bold, color: Colors.black), // Sesuaikan dengan gaya teks yang Anda inginkan
+            style: TextStyle(fontSize: 13, fontWeight: FontWeight.bold, color: Colors.black),
           ),
           SizedBox(height: 8),
           TextField(
             readOnly: true,
             decoration: InputDecoration(
-              hintText: value,
+              hintText: initialValue ?? value,
               border: OutlineInputBorder(
                 borderSide: BorderSide.none,
                 borderRadius: BorderRadius.circular(14),
               ),
               filled: true,
-              fillColor: whiteColor, // Sesuaikan dengan warna latar belakang yang Anda inginkan
+              fillColor: Colors.white,
             ),
           ),
         ],
