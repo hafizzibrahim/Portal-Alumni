5b87533f73963d28dab954fc195736fed3dc02e0
Hafiz Ibrahim <hafizzibrahimm0@gmail.com>
2024-06-25 11:47:11 +0700

    -update dashboard

--- lib/ui/pages/dashboard_page.dart
+++ lib/ui/pages/dashboard_page.dart
@@ -1,149 +1,251 @@
 part of 'pages.dart';
 
-class DashboardPage extends StatelessWidget {
+class DashboardPage extends StatefulWidget {
   const DashboardPage({super.key});
 
   @override
+  _DashboardPageState createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+  late Future<void> _fetchStatisticsFuture;
+
+  List<String> chartTitles = [
+    'Waktu Mulai Mencari Pekerjaan',
+    'Instansi Pekerjaan',
+    'Waktu Tunggu',
+    'Range Gaji Alumni',
+    'Kesesuaian Prodi dengan Pekerjaan',
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    final viewModel = Provider.of<DashoardViewmodel>(context, listen: false);
+    _fetchStatisticsFuture = viewModel.fetchStatistics();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final viewModel = Provider.of<DashoardViewmodel>(context);
+
     return Scaffold(
-      backgroundColor: whiteblueColor,
+      backgroundColor: Colors.blueGrey[50],
       appBar: PreferredSize(
         preferredSize: const Size.fromHeight(70),
         child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            decoration: BoxDecoration(
-                gradient: LinearGradient(colors: [lightBlue, darkBlue]),
-                borderRadius:
-                    const BorderRadius.vertical(bottom: Radius.circular(25))),
-            child: AppBar(
-              backgroundColor: Colors.transparent,
-              title: Text(
-                'Dashboard',
-                style:
-                    whiteTextStyle.copyWith(fontWeight: medium, fontSize: 20),
-              ),
-              leading: IconButton(
-                  onPressed: () => Navigator.of(context).pop(),
-                  icon: Icon(
-                    Icons.arrow_back_ios_new,
-                    size: 30,
-                    color: whiteColor,
-                  )),
-            )),
-      ),
-      body: ListView(
-        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 20),
-        children: [
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 32),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(14),
-              color: whiteColor,
-              boxShadow: [
-                BoxShadow(
-                    blurRadius: 5,
-                    spreadRadius: 1,
-                    offset: const Offset(0, 3),
-                    color: greyColor),
-              ],
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [lightBlue, darkBlue]),
+            borderRadius: const BorderRadius.vertical(bottom: Radius.circular(25)),
+          ),
+          child: AppBar(
+            backgroundColor: Colors.transparent,
+            title: Text(
+              'Dashboard',
+              style: TextStyle(color: Colors.white, fontWeight: FontWeight.w600, fontSize: 20),
             ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width / 2,
-                  child: Column(
-                    children: [
-                      Text(
-                        'Dashboard Tracer Study Untirta',
-                        style: blackTextStyle.copyWith(
-                            fontSize: 18, fontWeight: extraBold),
-                      ),
-                      const SizedBox(
-                        height: 8,
-                      ),
-                      Text(
-                        'Platform digital ini menghubungkan lulusan, memfasilitasi komunikasi, dan menyediakan informasi bermanfaat bagi alumni.',
-                        style: blackTextStyle.copyWith(
-                            fontSize: 10, fontWeight: reguler),
-                        textAlign: TextAlign.justify,
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: softGreyColor,
-                    boxShadow: [
-                      BoxShadow(
-                          blurRadius: 5,
-                          spreadRadius: 1,
-                          offset: const Offset(0, 3),
-                          color: greyColor),
-                    ],
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
+            leading: IconButton(
+              onPressed: () => Navigator.of(context).pop(),
+              icon: Icon(
+                Icons.arrow_back_ios_new,
+                size: 30,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: FutureBuilder(
+        future: _fetchStatisticsFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            return ListView.builder(
+              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+              itemCount: viewModel.statistics?.data.length ?? 0,
+              itemBuilder: (context, index) {
+                String key = viewModel.statistics!.data.keys.elementAt(index);
+                Statistic statistic = viewModel.statistics!.data[key]!;
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                      child: Row(
                         children: [
-                          Text(
-                            'Statistic',
-                            style: greyTextStyle.copyWith(
-                                fontSize: 10, fontWeight: medium),
+                          Container(
+                            height: 49,
+                            width: 20,
+                            decoration: BoxDecoration(color: orangeColor),
                           ),
-                          SizedBox(
-                            height: 6,
-                          ),
-                          Text(
-                            '85,250',
-                            style: blackTextStyle.copyWith(
-                                fontSize: 14, fontWeight: semiBold),
-                          ),
-                          SizedBox(
-                            height: 6,
-                          ),
-                          Text(
-                            '2.8%',
-                            style: greenTextStyle.copyWith(
-                                fontSize: 7, fontWeight: medium),
-                          ),
+                          Expanded(
+                            child: Container(
+                              width: MediaQuery.of(context).size.width,
+                              padding: const EdgeInsets.only(left: 15, top: 15, bottom: 15),
+                              decoration: const BoxDecoration(
+                                  gradient: LinearGradient(
+                                      colors: [Color(0xff0558A4), Color(0xff0038FF)],
+                                      begin: Alignment.centerLeft,
+                                      end: Alignment.centerRight)),
+                              child: Text(
+                                chartTitles[index],
+                                style: whiteTextStyle.copyWith(fontSize: 13, fontWeight: bold),
+                              ),
+                            ),
+                          )
                         ],
                       ),
-                      SizedBox(
-                        width: 8,
+                    ),
+                    _buildChart(statistic),
+                  ],
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
+  }
+
+  Widget _buildChart(Statistic statistic) {
+    switch (statistic.chartType.type) {
+      case 'bar':
+        return _buildBarChart(statistic);
+      case 'doughnut':
+        return _buildDoughnutChart(statistic);
+      default:
+        return Container();
+    }
+  }
+
+  Widget _buildBarChart(Statistic statistic) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        padding: EdgeInsets.all(16),
+        height: 300,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(14),
+        ),
+        child: BarChart(
+          BarChartData(
+            titlesData: FlTitlesData(
+              bottomTitles: AxisTitles(
+                sideTitles: SideTitles(
+                  showTitles: true,
+                  getTitlesWidget: (value, meta) {
+                    return Padding(
+                      padding: const EdgeInsets.only(top: 8.0),
+                      child: Text(
+                        statistic.counts[value.toInt()].label,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 8,
+                        ),
                       ),
-                      Image.asset(
-                        'assets/images/dummy_chart.png',
-                        scale: 2.5,
-                      )
-                    ],
-                  ),
+                    );
+                  },
                 ),
-              ],
+              ),
+              leftTitles: AxisTitles(
+                sideTitles: SideTitles(
+                  showTitles: true,
+                  getTitlesWidget: (value, meta) {
+                    return Text(
+                      value.toInt().toString(),
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 8,
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+            borderData: FlBorderData(show: false),
+            barGroups: statistic.counts.map(
+                  (data) {
+                int index = statistic.counts.indexOf(data);
+                return BarChartGroupData(
+                  x: index,
+                  barRods: [
+                    BarChartRodData(
+                      toY: data.count.toDouble(),
+                      color: Colors.blue,
+                      width: 24,
+                      borderRadius: BorderRadius.circular(2),
+                    ),
+                  ],
+                );
+              },
+            ).toList(),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDoughnutChart(Statistic statistic) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        padding: EdgeInsets.all(16),
+        height: 300,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(14),
+        ),
+        child: PieChart(
+          PieChartData(
+            sections: statistic.counts.map(
+                  (data) {
+                int index = statistic.counts.indexOf(data);
+                return PieChartSectionData(
+                  value: data.count.toDouble(),
+                  title: data.label,
+                  color: Colors.primaries[index % Colors.primaries.length],
+                  radius: 100,
+                  titleStyle: TextStyle(fontSize: 8, fontWeight: FontWeight.bold, color: Colors.white),
+                );
+              },
+            ).toList(),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget titleSection(BuildContext context, String title) {
+    return Row(
+      children: [
+        Container(
+          height: 49,
+          width: 20,
+          decoration: BoxDecoration(color: orangeColor),
+        ),
+        Expanded(
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            padding: const EdgeInsets.only(left: 15, top: 15, bottom: 15),
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [Color(0xff0558A4), Color(0xff0038FF)],
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight)),
+            child: Text(
+              title,
+              style: whiteTextStyle.copyWith(fontSize: 13, fontWeight: bold),
             ),
           ),
-          Image.asset('assets/images/dummy_line_chart.png'),
-          Align(
-              alignment: Alignment.topRight,
-              child: TextButton(
-                  onPressed: () {},
-                  child: Text('Lihat Semua',
-                      style: greyTextStyle.copyWith(fontSize: 12)))),
-          const ArticleWigets(
-            imageUrl: 'assets/images/profile_photo.png',
-            title: 'Mulyadi',
-            subtitle: '3 Nutrisi Penting yang Dibutuhkan Oleh  Pelari',
-            description:
-                'Itu adalah faktor penting yang harus adalah faktor penting yang harus aksdnkfkdnfkns',
-            date: '12 Mar',
-            time: '5 min',
-            articleImageUrl: 'assets/images/img_article_example.png',
-          ),
-        ],
-      ),
+        )
+      ],
     );
   }
 }
