fc0bc4a670d55b96a6d885470bc00be34ddbd481
Ahmad Nurdin <nurnurdinnur11@gmail.com>
2024-06-27 21:32:58 +0700

    Update JobsModel.dart

--- lib/models/JobsModel.dart
+++ lib/models/JobsModel.dart
@@ -3,12 +3,14 @@ class Job {
   final String title;
   final String description;
   final String content;
+  final String imagePath;
 
   Job({
     required this.id,
     required this.title,
     required this.description,
     required this.content,
+    required this.imagePath
   });
 
   factory Job.fromJson(Map<String, dynamic> json) {
@@ -17,6 +19,7 @@ class Job {
       title: json['title'],
       description: json['description'],
       content: json['content'],
+      imagePath: json['image_path'],
     );
   }
 }
