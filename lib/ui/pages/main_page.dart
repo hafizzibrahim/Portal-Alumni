11a7ea0f617243196ab3ca5aaababf15ae2d653b
Hafiz Ibrahim <hafizzibrahimm0@gmail.com>
2024-06-28 09:29:48 +0700

    -update fix bottom nav bar

--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -1,6 +1,8 @@
 part of 'pages.dart';
 
 class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
   @override
   _NavigationExampleState createState() => _NavigationExampleState();
 }
@@ -16,9 +18,9 @@ class _NavigationExampleState extends State<MainPage> {
       body: IndexedStack(
         index: currentPageIndex,
         children: const <Widget>[
-           HomePage(),
-           TracerStudyPage(),
-           ProfilePage(),
+          HomePage(),
+          TracerStudyPage(),
+          ProfilePage(),
         ],
       ),
       bottomNavigationBar:
@@ -34,50 +36,45 @@ class _NavigationExampleState extends State<MainPage> {
             ),
           ),
           // Actual navigation bar container
-          Padding(
-            padding: const EdgeInsets.only(bottom: 20, left: 16, right: 16),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(50),
-              child: Container(
-                child: NavigationBarTheme(
-                  data: NavigationBarThemeData(
-                    indicatorColor: whiteblueColor,
-                    labelTextStyle: WidgetStateProperty.all(
-                      TextStyle(color: blueColor),
-                    ),
-                    iconTheme: WidgetStateProperty.all(
-                      IconThemeData(color: blueColor),
-                    ),
-                  ),
-                  child: NavigationBar(
-                    backgroundColor: whiteColor,
-                    labelBehavior: labelBehavior,
-                    selectedIndex: currentPageIndex,
-                    onDestinationSelected: (int index) {
-                      setState(() {
-                        currentPageIndex = index;
-                      });
-                    },
-                    destinations: <Widget>[
-                      NavigationDestination(
-                        icon: Icon(Icons.home_outlined, color: blueColor),
-                        label: 'Home',
-                        selectedIcon: Icon(Icons.home, color: blueColor),
-                      ),
-                      NavigationDestination(
-                        icon: Icon(Icons.article_outlined, color: blueColor),
-                        label: 'Form',
-                        selectedIcon:
-                            Icon(Icons.article_rounded, color: blueColor),
-                      ),
-                      NavigationDestination(
-                        icon: Icon(Icons.person_outline, color: blueColor),
-                        label: 'Profile',
-                        selectedIcon: Icon(Icons.person, color: blueColor),
-                      ),
-                    ],
-                  ),
+          ClipRRect(
+            borderRadius: const BorderRadius.only(topLeft: Radius.circular(25), topRight: Radius.circular(25)),
+            child: NavigationBarTheme(
+              data: NavigationBarThemeData(
+                indicatorColor: whiteblueColor,
+                labelTextStyle: WidgetStateProperty.all(
+                  TextStyle(color: blueColor),
                 ),
+                iconTheme: WidgetStateProperty.all(
+                  IconThemeData(color: blueColor),
+                ),
+              ),
+              child: NavigationBar(
+                backgroundColor: whiteColor,
+                labelBehavior: labelBehavior,
+                selectedIndex: currentPageIndex,
+                onDestinationSelected: (int index) {
+                  setState(() {
+                    currentPageIndex = index;
+                  });
+                },
+                destinations: <Widget>[
+                  NavigationDestination(
+                    icon: Icon(Icons.home_outlined, color: blueColor),
+                    label: 'Home',
+                    selectedIcon: Icon(Icons.home, color: blueColor),
+                  ),
+                  NavigationDestination(
+                    icon: Icon(Icons.article_outlined, color: blueColor),
+                    label: 'Form',
+                    selectedIcon:
+                    Icon(Icons.article_rounded, color: blueColor),
+                  ),
+                  NavigationDestination(
+                    icon: Icon(Icons.person_outline, color: blueColor),
+                    label: 'Profile',
+                    selectedIcon: Icon(Icons.person, color: blueColor),
+                  ),
+                ],
               ),
             ),
           ),
